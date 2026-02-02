cc88238a64a132dd28d77cdab01b6e711a0e539d
MsKrishna75 <kyadav6345@gmail.com>
2026-02-02 16:08:00 +0530

    api added in frontend

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import requests
 st.set_page_config(page_title="Car Price Prediction", page_icon="🚗", layout="centered")
 
 API_URL = (
-    # "https://car-prediction-lpfl.onrender.com/predict"
-    # or
+    "https://car-prediction-18i2.onrender.com/predict"
+    or
       "http://127.0.0.1:8000/predict"
 )  # change if your endpoint differs
 
